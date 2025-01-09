d1082b5032a2d3811230a83fe303b274797bd2b2
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2025-01-09 10:18:32 -0300

    Create methods to insert, update and remove user

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@ namespace dev_blog_dotnet.Models;
 [Table("[User]")]
 public class User
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
--- Program.cs
+++ Program.cs
@@ -7,10 +7,25 @@ class Program
 {
     private const string CONNECTION_STRING = @"Server=127.0.0.1,1433;Database=dev-blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true";    
 
-    private static void Main(string[] args)
+    private static void Main()
     {
-        var users = GetUsers();
-        var getUse = GetUserById(1);
+        var allusers = GetUsers();
+        Console.WriteLine($"{allusers.Count} users");
+
+        // var fistUser = GetUserById(1);
+        var newUser = new User(){
+            Id = 5,
+            Name = "John Doe [edited]",
+            Email = "johndoe2@dev.com",
+            Bio = "Front-end developer",
+            Image = "https://image.png",
+            PasswordHash = "hashpass",
+            Slug = "john-doe-2"
+        };
+
+        // CreateUser(newUser);
+        // UpdateUser(newUser);
+        // DeleteUser(2);
     }
 
     private static IList<User> GetUsers()
@@ -24,5 +39,21 @@ class Program
         using var connection = new SqlConnection(CONNECTION_STRING);
         var user = connection.Get<User>(userId);
         return user;
+    }
+
+    private static void CreateUser(User user) {
+        using var connection = new SqlConnection(CONNECTION_STRING);
+        connection.Insert<User>(user);
+    }
+
+    private static void UpdateUser(User user) {
+        var connection = new SqlConnection(CONNECTION_STRING);
+        connection.Update<User>(user);
+    }
+
+    private static void DeleteUser(int userId) {
+        var connection = new SqlConnection(CONNECTION_STRING);
+        var userToBeDeleted = connection.Get<User>(userId);
+        connection.Delete<User>(userToBeDeleted);
     }
 }
